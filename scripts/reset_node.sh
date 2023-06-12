53dfca44a09ec2530e82b3752473a60116799040
j0sh3rs <j0sh3rs@noreply.github.com>
2023-06-12 09:39:43 -0400

    Fix and encrypt all the things

--- scripts/reset_node.sh
+++ scripts/reset_node.sh
@@ -6,5 +6,5 @@ ip link delete cilium_vxlan
 iptables-save | grep -iv cilium | iptables-restore
 ip6tables-save | grep -iv cilium | ip6tables-restore
 
-k3s-uninstall.sh 
+k3s-uninstall.sh
 rm -rf /var/lib/rancher /etc/rancher /var/lib/longhorn /etc/cni/ /opt/cni /var/lib/cni
