3a44174d612a55a1f1234428ef423e4f092bcdfc
j0sh3rs <github@beholdthehurricane.com>
2023-10-17 11:43:06 -0400

    Readd longhorn

--- scripts/bpftune.sh
+++ scripts/bpftune.sh
@@ -3,7 +3,7 @@ set -eo pipefail
 
 echo "Install pre-requisites for make"
 
-apt-get install -y libbpf1 libbpf-tools libbpfcc-dev libbpfcc libbpf-dev libcap2 libcap2-bin libcap-dev libnl-3-dev libnl-3-200 clang-17 clang clang-tools-17 clang-tools python3-docutils llvm llvm-17-tools llvm-17 llvm-17-dev libnl-genl-3-dev libnl-genl-3-200 libnl-route-3-200 libnl-route-3-dev make
+apt-get install -y libbpf1 libbpf-tools libbpfcc-dev libbpfcc libbpf-dev libcap2 libcap2-bin libcap-dev libnl-3-dev libnl-3-200 clang-17 clang clang-tools-17 clang-tools python3-docutils llvm llvm-17-tools llvm-17 llvm-17-dev libnl-genl-3-dev libnl-genl-3-200 libnl-route-3-200 libnl-route-3-dev make linux-tools-generic linux-cloud-tools-generic
 
 mkdir -p ~/git/oracle
 cd ~/git/oracle
@@ -12,3 +12,4 @@ cd bpftune
 make
 make install
 systemctl start bpftune
+systemctl enable bpftune
