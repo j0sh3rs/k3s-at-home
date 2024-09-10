05938a436b2c90134e779e3b9fc9a517e41c4f9c
j0sh3rs <github@beholdthehurricane.com>
2024-09-10 16:23:40 -0400

    Stash script changes, and redeploy uptime-kuma

--- scripts/bootstrap_agents.sh
+++ scripts/bootstrap_agents.sh
@@ -1,5 +1,5 @@
 {
-    "data": "ENC[AES256_GCM,data:qpKZsKdHLzthRh1TEvmZFjUXZg+uTPScJzHWhju3t0Khgy+OnAKcVBoCnkitJkZeLbn/xV5iR8cKTcBCXKe/9h8wQz0Azk85Y3Izz0P6HhxuPYjaK+Tk+oNPiHUOQkaOoPzSPlzI7RjPwWxBA/8KqRJHhb5pwonVBw866vnBdT7x/SpBByJBAahWhHht8UiehE4lONwLCUPLLcK1leNuwaddFKVV+LtjGf1H5ML1S6c3dVZ0LqAHb+N6THcJmMyfxTdfjJyQc/qIqAxPeb3wM1ikPTtxXbvtqhD+k36Jxmj3AbfpANBa/yjGAwiV8SV3UtFYwjTzI9YVb5ltReog1EPa9LZA26AreXsqgSso4D//mrx1Hc4gDcp/1/QIDnB6aG4p+ijl2/Hh5/bCq3lnQgZ22RSByGh6MzE47yTALnc05RBsGy82k1WZ2D0JRP7e//yhQJ9LwFv2sNG7vGHL3+lTQVqBZ5X9mpCmO0IjvDKSy2RJlJDOcskc3/q+02LETXgxTAwEMlwrVaAR7grN1bCWwZP2z/wJtdU0QHV6dn0ck9Xd0O2gNke7foH0W94sQusa60q+yYy/nFD1tuB7llaGxQMphSc0vwXIqvjGLat2RGa+Z4xXMYS4GGjIpUZD4V2Ufpocpd0B4ZXwfw==,iv:qrPisgdREzokMBXodnuCQ3zulE2kGmvDPhAIvTsUnUc=,tag:fQ7IiQn76+N4YKphml1KqQ==,type:str]",
+    "data": "ENC[AES256_GCM,data:CSHnK5SaOtPYx7h1ngLitb+BCwH54ROYLFTQs1/A5zDWu/NfFcOhwoYc4j9PxDXZOPSveKXv3befYX8fZwCKmG7nW/ML5VN7Zdv9PhHDhGh+INwdtiwJnWJN3UHOOb+6iJBExVC6jMSM9Cn2bcMdckCZU7iJ0IQ8LgMMHIALe1UDJJxoAy8Cycimv8wb7tTA+eSlYoOVENF44D0AY17D1eNDDO2fkuyaV+LDLqjbL2sjCUfvX3CooY7XcvCjHRsTew3eld+h4GmUy5I3t0ZEWztFHF5Hm5fAV3gDuaAFD6tNCWP6jg0IYTR60FigiZ0QM1vmBLAvMbM1Bk30aZVj+VMTCaAsbqSpdDWrRTmsGiLOzaya5kW3QULz1WTe/FEr5wx8xe1hZF5zPCUkUqH7zsl3P2XQz9qoQP1wisFDLAaLm0s5yyrFLf1kt9ShJFk5Z0D4bFhoXAqlEVAgNHUJz/vPNUEq2ArVIuXpYUmfZIxGXLrQKzDsttWa4K/Pf8jkXdMB3Eqz3fYKm1NCYlKxeWX4Gnhjuvbmo4tG/KFKwpwsv4APySw6DDUa9/2vj8XyV3rwqboM37OqNSSUZztvnkJalTle3DjlkkQOsvC/gBbPsMMtBMztN7kvKJPV4a+cIaETEdPnQy62uA1EslkOWRJCVS2d+jyVYzkeTV6E9FZdQBYm1cbZWqGV9+PIMvQlD0K3tV9h67mYnm1oqu+TcNLKCvLt0TV+Oo1sV9nTeeWlQ1WrsYUpCrt7l6sGtlFUZKGhE+2vFTvBP7MWAKkC9SvkQtHAzidrf5T+ZYllaJG0Q7DSkW3+Neue1JBfcQpdr++PjmGnJJIRQWvjjZCm6ZV17BKq,iv:hDGgLFqtf0BYds+rY+Up4Sxox2qMbm0uUiiy/iJOl84=,tag:iCrbg4WDTUq3PyW1eH9hnw==,type:str]",
     "sops": {
         "kms": null,
         "gcp_kms": null,
@@ -8,13 +8,13 @@
         "age": [
             {
                 "recipient": "age1qwwzsz6z2mmu6hpmjt2he7nepmnhutmhehvkva7l5zy5xzf08d5s5n4d6n",
-                "enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSA3OFB0NmZwVVFJbzd2Y1ZN\nK1Q3V0h3S1lPT1VDWG1kVVJpMzQ2L2l0aGowCkdwczZ3YWVJSW4xdHpPVkc3NmRH\nMy9WM20wdTNiWUpNb0FURk1uWWh0cG8KLS0tIFNLM2N4VVdzRHk2M01vWHBIUkR5\nMTh3cGJ6VXpTN01uYWNKbDJrMHpkMGcKWCc0PcEuZbif1bXw+U4Rn+AtKsK9AKC/\n98a67zl0OOAFhbNWdcNY4mSELoXWyQbC1gGnnt8q6owJjT8BL5+xow==\n-----END AGE ENCRYPTED FILE-----\n"
+                "enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSAyVXhybE1YWFZBTm5rQlJw\naEcrY01pa2ErYVZHYVhvZy9OekxSV3NyOEdNClR2NUJDeDVhYzkxRjVQQzA0bnJL\nL0Z0Wmk2SmNZWGN5K1RCVHd0ZE14SWsKLS0tIDJHNTliVytkeW5QZXF2OGFkZlBX\nUnFRM2V2R2txZFQ1eFp4UmNuZm9adzAKn/DhnBPJn2vzdQcFWh7h3r2OMaSQcdsk\nbKqGi9ERPEiZ6yRot4CPW/NaPYE7kPLVn4/oAzot+gAIOalK5FmDzw==\n-----END AGE ENCRYPTED FILE-----\n"
             }
         ],
-        "lastmodified": "2023-10-16T18:35:46Z",
-        "mac": "ENC[AES256_GCM,data:bfxI89x3wKKG+0qkTO8xX8ejZ0vHxu5fwdvpS3N3TEL1QY/lV5eAWtPoJj3crDQuvFf+i/EaFEN5nYEonB1+ER5jgKzz9/3JLcdRDbNHmcY7lKfmlCrRigdUNzciEhDvCgo5I/1bo3BlDEmwl3N4v5hXGbwO/bYo6GS8wXlc/NI=,iv:ocE96EFMXgHuZ5sCKpcm9CSYuelwbJH9iahTcGuoNgY=,tag:lOL5lNKSrMapAB3uKlbjCA==,type:str]",
+        "lastmodified": "2024-09-04T13:43:30Z",
+        "mac": "ENC[AES256_GCM,data:Ra8P0VBiT+PsrRMykyi1AnfMMY8hTgdLxnle9PapgEDDee638Cjq5FS4SHQA+KyGrFI2rb8D3ZBVl9b966tWmmWLCZDUUVaEE3Xnh/FFNIie1RanNZ50RturGfGa5mJWuGzBKHFjWd+6bN58DeANxeaSFrVNc/7nfxwCHqS5Ocw=,iv:PCzfGhxMrDgvwS6joK7gFkLNoJCIUkE3nqlc0GyV2a8=,tag:keIXXaWY8edOLU7SqWEVYw==,type:str]",
         "pgp": null,
-        "encrypted_regex": "^(data|stringData|k8sServiceHost)$",
-        "version": "3.8.1"
+        "encrypted_regex": "^(data|stringData|Authorization)$",
+        "version": "3.9.0"
     }
 }
